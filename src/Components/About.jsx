e51859dd1a1ea323f353e996ed0ef7c1b1e1a6a7
Niki Kayastha <niki.kstha7664@gmail.com>
2023-09-23 13:13:50 +0545

    changing skill list

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -33,10 +33,8 @@ const description =
 const skillsList = [
   "Web design",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web testing",
+  "API testing",
 ];
 
 /**
